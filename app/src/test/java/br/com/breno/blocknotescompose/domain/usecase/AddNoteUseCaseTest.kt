3a6ef2ca147bc533a75422eef23dfc4dd5485f33
breno <brenorocha947@gmail.com>
2023-06-15 13:24:33 -0300

    Adding useCase tests

---
+++ app/src/test/java/br/com/breno/blocknotescompose/domain/usecase/AddNoteUseCaseTest.kt
@@ -0,0 +1,35 @@
+package br.com.breno.blocknotescompose.domain.usecase
+
+import br.com.breno.blocknotescompose.data.model.NoteModel
+import br.com.breno.blocknotescompose.domain.repository.AddNoteRepository
+import br.testing_base.rules.TestCoroutineRule
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class AddNoteUseCaseTest {
+
+    @get:Rule
+    val coroutineRule = TestCoroutineRule()
+
+    private val repository: AddNoteRepository = mockk(relaxed = true)
+    private val useCase = AddNoteUseCase(repository)
+
+    @Test
+    fun `When useCase is invoke Should return a Long`() {
+        // Given
+        val noteModel = NoteModel()
+        val long = 1L
+        coEvery { repository.addNote(noteModel) } returns long
+        // When
+        coroutineRule.runBlockingTest {
+            val result = useCase.invoke(noteModel)
+
+            assertEquals(long, result)
+        }
+     }
+}
---
+++ app/src/test/java/br/com/breno/blocknotescompose/domain/usecase/FetchNotesUseCaseTest.kt
@@ -0,0 +1,36 @@
+package br.com.breno.blocknotescompose.domain.usecase
+
+import br.com.breno.blocknotescompose.data.model.NoteModel
+import br.com.breno.blocknotescompose.domain.repository.FetchNotesRepository
+import br.testing_base.rules.TestCoroutineRule
+import io.mockk.coEvery
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class FetchNotesUseCaseTest {
+
+    @get:Rule
+    val coroutineRule = TestCoroutineRule()
+
+    private val fetchNotesRepository: FetchNotesRepository = mockk(relaxed = true)
+    private val useCase = FetchNotesUseCase(fetchNotesRepository)
+
+    @Test
+    fun `When useCase is invoke Should return a list of note model`() {
+        // Given
+        val noteModel = listOf(NoteModel(), NoteModel())
+        coEvery { fetchNotesRepository.fetchNotes() } returns flowOf(noteModel)
+        // When
+        coroutineRule.runBlockingTest {
+            val result = useCase()
+
+            assertEquals(noteModel, result.first())
+        }
+     }
+}
